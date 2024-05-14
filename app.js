4536213f01caf1ece53a8bbe9efef232a450d7e0
SUHAIB SABIR <suhaibsabir110@gmail.com>
2024-05-13 22:07:46 -0700

    n
    n

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ b.addEventListener("click", function change() {
     if (flag == 0) {
         b.innerHTML = `CLICK TO UNBUY`
         a.innerHTML = `YOU SELECTED THIS CAR`
+        b.style = "none"
         flag = 1
     }
-    else if (flag == 1) {
+    else {
+        b.style.backgroundColor = "red"
         b.innerHTML = `CLICK TO BUY`
         a.innerHTML = `CLICK TO BUY BELOW`
         flag = 0;
